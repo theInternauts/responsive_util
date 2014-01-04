bea0955ee9db0fe6a98b6e46b9f6641d82635f81
theInternauts <cricketw@wearetheinternauts.com>
2014-01-03 23:53:24 -0800

    added destroy() method to remove appened DOM nodes and nullify the plugin from future runtime calls

--- JQuery_responsive_util.js
+++ JQuery_responsive_util.js
@@ -20,7 +20,7 @@
     colors: [],
     size: 0.5,
     nodes: "<div id='responsive-check'><div class='msg' id='screen'><p>screen</p></div><div class='msg' id='tablet'><p>tablet</p></div><div class='msg' id='phone'><p>phone</p></div></div>",
-    styles: "<style>#responsive-check{visibility: hidden;display: none;}.msg {visibility: hidden;display:none;position: absolute;z-index: 100000000000000000000;right: 1em;top: 1em;width: 4em;height: 4em;text-align: center;border: 3px solid black;background-color: white;color: black;font-size: 0.5em;}@media only screen and (max-width: 34.99em){#responsive-check{visibility: visible;display: block;}#phone {visibility: visible;display: block;}}@media only screen and (min-width: 35em) and (max-width: 70em){/* Style adjustments for viewports that meet the condition */#responsive-check{visibility: visible;display: block;}#tablet {visibility: visible;display: block;border: 3px solid red;background-color: white;color: red;}}@media only screen and (min-width: 70.01em) {#responsive-check{visibility: visible;display: block;}#screen {visibility: visible;display: block;border: 3px solid blue;background-color: white;color: blue;}}</style>"
+    styles: "<style id='responsive-style'>#responsive-check{visibility: hidden;display: none;}.msg {visibility: hidden;display:none;position: absolute;z-index: 100000000000000000000;right: 1em;top: 1em;width: 4em;height: 4em;text-align: center;border: 3px solid black;background-color: white;color: black;font-size: 0.5em;}@media only screen and (max-width: 34.99em){#responsive-check{visibility: visible;display: block;}#phone {visibility: visible;display: block;}}@media only screen and (min-width: 35em) and (max-width: 70em){/* Style adjustments for viewports that meet the condition */#responsive-check{visibility: visible;display: block;}#tablet {visibility: visible;display: block;border: 3px solid red;background-color: white;color: red;}}@media only screen and (min-width: 70.01em) {#responsive-check{visibility: visible;display: block;}#screen {visibility: visible;display: block;border: 3px solid blue;background-color: white;color: blue;}}</style>"
   }
 
   $.fn.responsive_util.hide = function(){ 
@@ -30,5 +30,13 @@
   $.fn.responsive_util.show = function(){ 
     $('#responsive-check').animate({ opacity: '1', visibility: 'visible', display: 'block' }, 600)
   }
+
+  $.fn.responsive_util.destroy = function(){ 
+    $('#responsive-check').remove()
+    $('#responsive-style').remove()
+    $.fn.responsive_util = undefined //not sure is this line is a good idea
+  }
+
+  
 
 })( jQuery, window, document );
